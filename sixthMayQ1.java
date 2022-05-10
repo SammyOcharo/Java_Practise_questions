1edc6ae02e18c6c97402967104823c4c6a08975a
sammy ocharo <obanyisammy@gmail.com>
2022-05-11 00:12:57 +0300

    6th may quiz

---
+++ sixthMayQ1.java
@@ -0,0 +1,39 @@
+//Write a program to sort an integer array
+
+import java.util.Arrays;
+
+public class sixthMayQ1 {
+    public static void main(String args[]) {
+        //testing our bubble sort method in Java
+        int[] unsorted = {32, 39,21, 45, 23, 3};
+        bubbleSort(unsorted);
+
+        //one more testing of our bubble sort code logic in Java
+        int[] test = { 5, 3, 2, 1};
+        bubbleSort(test);
+
+        //one more testing of our bubble sort code logic in Java
+        int[] test2 = { 5, 3, 2, 1, 2, 7, 9, 8 ,10};
+        bubbleSort(test2);
+    }
+
+
+    public static void bubbleSort(int[] unsorted){
+        System.out.println("The unsorted array is" + Arrays.toString(unsorted));
+
+        for (int i=0; i<unsorted.length-1; i++){
+            for (int j=1; j< unsorted.length-i; j++){
+                if (unsorted[j-1] > unsorted[j]){
+                    int temp = unsorted[j];
+                    unsorted[j]=unsorted[j-1];
+                    unsorted[j-1]=temp;
+                }
+
+            }
+            System.out.printf("unsorted array after %d pass %s: %n", i+1, Arrays.toString(unsorted));
+        }
+
+
+    }
+
+}
---
+++ sixthMayQ2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/** * Simple Java Program to check or find if a number is Armstrong number or not.
+ * * An Armstrong number of three digit is a number whose sum of cubes of its digit is equal
+ * * to its number. For example 153 is an Armstrong number * of 3 digit because 1^3+5^3+3^3 or 1+125+27=153
+ * * */
+
+
+public class sixthMayQ2 {
+    public static void main(String[] args) {
+
+        //Prompt user to enter 3 digit number to check if its an Armstrong number
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number to check if its Armstrong: \n");
+
+        int number = input.nextInt();
+
+        if (Armstrong(number)){
+            System.out.println(number + " is an ArmStrong number");
+        }else {
+            System.out.println( number + " is not an ArmStrong number");
+        }
+
+
+    }
+
+    private static boolean Armstrong(int number){
+        int originalNumber = number;
+        int finalNumber = 0;
+
+        while(number !=0){
+            int remainder = number%10;
+            finalNumber = finalNumber+remainder*remainder*remainder;
+            number = number/10;
+        }
+        if(originalNumber==finalNumber){
+            return true;
+        }
+        return false;
+    }
+}
