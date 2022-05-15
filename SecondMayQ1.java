c770982f3289071d19ce38acf803447a4d64f79f
sammy ocharo <obanyisammy@gmail.com>
2022-05-15 08:58:08 +0300

    2nd may quiz

---
+++ SecondMayQ1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//make a java program to determine sunny numbers(this re numbers when you add 1 to it makes the number a square number)
+public class SecondMayQ1 {
+    public static void main(String[] args) {
+        System.out.print("Input number to check weather it is a sunny number: ");
+        int number = new Scanner(System.in).nextInt();
+
+        if (Sunny(number)){
+            System.out.println("the number "+ number + " is a sunny number");
+        } else {
+            System.out.println("the number "+ number + " is not a sunny number");
+        }
+
+    }
+
+    public static boolean Sunny(int number){
+        int determiner = number + 1;
+        double Squareroot =  Math.sqrt(determiner);
+        if(Squareroot-Math.floor(Squareroot)==0){
+            return true;
+        }
+        return false;
+    }
+}
