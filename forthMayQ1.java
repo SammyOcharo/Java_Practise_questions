35dc31bb1da66f12bbd408ea8e7671d6c53427b4
sammy ocharo <obanyisammy@gmail.com>
2022-05-16 17:25:18 +0300

    4th may quiz

---
+++ forthMayQ1.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+//java program to copy all elements of an array to another array
+public class forthMayQ1 {
+    public static void main(String[] args) {
+        int [] mainArray =new int [] {2,3,4,8,5,7,6,45,85,98,41};
+
+
+        System.out.println("The Original Array is "+ Arrays.toString(mainArray));
+        System.out.println("The Original Array is "+ CopiedArray(mainArray));
+    }
+
+    static String CopiedArray(int[] mainArray){
+        int [] copiedArray = new int[mainArray.length];
+        for (int i = 0; i < copiedArray.length ; i++) {
+            copiedArray[i] = mainArray[i];
+        }
+        return Arrays.toString(copiedArray);
+    }
+}
---
+++ forthMayQ2.java
@@ -0,0 +1,36 @@
+//java program to find the frequency of each element in an array
+public class forthMayQ2 {
+    public static void main(String[] args) {
+        int [] arr = new int[]{1,2,8,3,2,2,2,5,1};
+
+        int [] frequency = new int[arr.length];
+
+        int visited = -1;
+        for (int i = 0; i < arr.length ; i++) {
+            int count = 1;
+
+            for (int j = i+1; j < arr.length ; j++) {
+                if (arr[i]==arr[j]){
+                    count++;
+                    //To avoid counting the same element again
+                    frequency[j] = visited;
+                }
+
+            }
+            if (frequency[i] != visited) {
+                frequency[i] = count;
+            }
+        }
+        // Displaying each element alongside its frequency of appearance
+        System.out.println("-----------------------------");
+        System.out.println("Element | Frequency");
+        System.out.println("-----------------------------");
+
+        for (int i = 0; i < frequency.length ; i++) {
+            if (frequency[i] != visited){
+                System.out.println("      "+ arr[i] + " |       "+ frequency[i]);
+            }
+        }
+        System.out.println("-----------------------------");
+    }
+}
