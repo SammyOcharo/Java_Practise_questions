bdc537073e2ead5b953da65f74632b7fbcc34d1b
sammy ocharo <obanyisammy@gmail.com>
2022-05-18 23:35:52 +0300

    13th may quiz(removing duplicate elements in an array)

--- May13thQ2.java
+++ May13thQ2.java
@@ -1,6 +1,30 @@
+import java.util.Arrays;
+
 //java program to remove duplicate elements in an array
 public class May13thQ2 {
     public static void main(String[] args) {
 
+        int [] arr = new int[] {10,10,50,50,20,30,40,30,60};
+        System.out.println("The array before removing the duplicate elements " + Arrays.toString(arr));
+
+        System.out.print("The array after removing the duplicates is ");
+        removeDuplicates(arr);
+    }
+    static void removeDuplicates(int[] arr){
+        int [] arr2 = new int[9];
+        int visited = -1;
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = i+1; j < arr.length ; j++) {
+              if(arr[i]==arr[j]){
+                  arr[j] = visited;
+              }
+            }
+
+        }
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] !=visited){
+                System.out.print(arr[i]+ ",");
+            }
+        }
     }
 }
