551a31ffaacf8bcdc68002fda57a0c44c1a2a2b0
sammy ocharo <obanyisammy@gmail.com>
2022-05-20 09:50:04 +0300

    20th may quiz 2 (inputing a matrix and calculating the sum of elements in a row and column)

--- May20thQ2.java
+++ May20thQ2.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 //Java program to find the sum of each row and each column of a matrix
 public class May20thQ2 {
     public static void main(String[] args) {
-        int [][] matrix1 = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
-        };
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the number of rows: ");
+        int rows = in.nextInt();
+
+        System.out.print("Input the number of columns: ");
+        int columns = in.nextInt();
+
+        int [][] matrix1 = new int[rows][columns];
+
+        System.out.println("Input the matrix values and press enter after each input");
+        for (int i = 0; i < rows ; i++) {
+            for (int j = 0; j < columns ; j++) {
+                matrix1[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println("The input matrix is ");
+        for (int i = 0; i < rows ; i++) {
+            for (int j = 0; j < columns ; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         int row1 = matrix1.length;
         int col1 = matrix1[0].length;
